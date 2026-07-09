bfe6899c261893bf2ca329d820a8bc83830113f2
Marishwari <marishwari20@gmail.com>
2026-07-09 11:54:34 +0530

    Wishlist update

--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Wishlist = require("../models/Wishlist");
+const Wishlist = require("../models/wishlist");
 const {protect} = require("../middleware/authMiddleware");
 
 const router = express.Router();
